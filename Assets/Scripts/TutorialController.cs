bae53c6cf0185bfe0ddf5a59958b9d71b9b89243
Rex <1780692450@qq.com>
2020-11-04 13:23:44 -0500

    Finish Menu

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialController : MonoBehaviour
+{
+    
+    public void Menu()
+    {
+        //SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+
+}
