ca6566cfb6fbacdd678635f8b41fc85081d2e8a8
Gurwinder Singh <sainirebel@gmail.com>
2020-11-03 22:12:40 -0500

    Parallax Script
    parallaxEffect changes to parallax_Effect

--- Assets/Scripts/Parallex.cs
+++ Assets/Scripts/Parallex.cs
@@ -6,7 +6,7 @@ public class Parallex : MonoBehaviour
 {
     private float length, startpos;
     public GameObject cam;
-    public float parallaxEffect;
+    public float parallax_Effect;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,8 @@ public class Parallex : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallax_Effect));
+        float dist = (cam.transform.position.x * parallax_Effect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
         if (temp > startpos + length) startpos += length;
