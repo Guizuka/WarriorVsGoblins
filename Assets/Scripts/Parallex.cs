56ebcddb01c94220d866cbb926234a5ad7f8d205
Gurwinder Singh <sainirebel@gmail.com>
2020-11-04 19:35:30 -0500

    Assets Added
    Obstacles, new platform , environment and door  added

--- Assets/Scripts/Parallex.cs
+++ Assets/Scripts/Parallex.cs
@@ -6,7 +6,7 @@ public class Parallex : MonoBehaviour
 {
     private float length, startpos;
     public GameObject cam;
-    public float parallax_Effect;
+    public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,8 @@ public class Parallex : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallax_Effect));
-        float dist = (cam.transform.position.x * parallax_Effect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
         if (temp > startpos + length) startpos += length;
